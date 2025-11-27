934a80ed70cb31e3ad833d9315518a7158f31149
fredericotonus <fredericotonus@gmail.com>
2025-11-27 12:54:25 +0100

    Cleaning the code

--- plot1.R
+++ plot1.R
@@ -1,14 +1,8 @@
-library("data.table")
+library(data.table)
 
 data <- fread("./household_power_consumption.txt", sep=";", header=TRUE, 
               na.strings="?")[Date == "1/2/2007" | Date == "2/2/2007", ]
 dim(data)
-
-data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
-class(data$Date)
-
-data$Time <- strptime(data$Time, "%H:%M:%S")
-class(data$Time)
 
 hist(data$Global_active_power, 
      main = "Global Active Power", 
