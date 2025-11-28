8ab251b7c5b9ab83742c0dc7be4ccbcf75c342d7
fredericotonus <fredericotonus@gmail.com>
2025-11-28 17:01:37 +0100

    commenting the code

--- plot1.R
+++ plot1.R
@@ -1,14 +1,15 @@
+# reading the data with fread function from data.table package
+# in order to read just a subset of it
 library(data.table)
-
 data <- fread("./household_power_consumption.txt", sep=";", header=TRUE, 
               na.strings="?")[Date == "1/2/2007" | Date == "2/2/2007", ]
-dim(data)
 
+# plotting the histogram
 hist(data$Global_active_power, 
      main = "Global Active Power", 
      xlab = "Global Active Power (kilowatts)",
      col = "red")
 
+# copying the plot to a png file and turning the connection off
 dev.copy(png, file = "plot1.png")
 dev.off()
-
